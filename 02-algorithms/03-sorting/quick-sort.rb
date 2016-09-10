2269e8f1e8f6391dbfe881ecdc1548ca087bca29
Russell Schmidt <russell.schmidt@me.com>
2016-09-09 21:15:48 -0700

    quicksort done

--- 02-algorithms/03-sorting/quick-sort.rb
+++ 02-algorithms/03-sorting/quick-sort.rb
@@ -2,21 +2,31 @@ def quick_sort(collection)
 	pivot_index = collection.length - 1
 	pivot = collection[pivot_index]
 
-	for i in 0..pivot_index
-		if collection[i] > collection[pivot_index]
-			collection.insert(pivot_index + 1, collection[i])
-			collection[i] = nil
+	if collection.length <= 1
+		collection
+	else
+		for i in 0..pivot_index
+			if collection[i] > collection[pivot_index]
+				collection.insert(pivot_index + 1, collection[i])
+				collection[i] = nil
+			end
 		end
+		collection.compact!
+
+		# split the array
+		# recursively call quick_sort on the subarray
+		pivot_index = collection.index(pivot)
+		left = quick_sort(collection[0...pivot_index])
+		right = quick_sort(collection[(pivot_index + 1)...collection.length])
 	end
-	collection.compact!
-
-	# split the array
-	
-
-	collection
+	collection = [left] + [pivot] + [right]
+	collection.compact
 end
 
 
 arr = ["E","C","A","B","F","G","D"]
-
+puts " before "
+puts arr
+puts " after "
 puts quick_sort(arr)
+puts " -- "
